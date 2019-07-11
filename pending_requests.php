5d477c00a54efd6461ffe61ef0ec3742b80e6cdd
Spriha <spriha@mindfiresolutions.com>
2019-07-11 12:36:57 +0530

    modified pagination

--- pending_requests.php
+++ pending_requests.php
@@ -15,10 +15,11 @@
 		$conn = $obj->connect('localhost','php_project',$db_username,$db_password);
 		$query = "SELECT user_type FROM user_types WHERE user_type != 'Admin'";
 		$result = $obj->select_records($query);
+
 	    echo '<nav class="navbar navbar-transparent justify-content-center">
 	    		<form class="form-inline" method="POST" action="">
 				  	<div class="form-group">
-						<input type="text" class="form-control" id="search" value="'.$_POST['search'].'" placeholder="Enter first name" name="search">
+						<input type="text" class="form-control" id="search" value="'.($_POST['search']?$_POST['search']:$_GET['s']).'" placeholder="Enter first name" name="search">
 			      		<button class="btn btn-success form-control mr-sm-2" type="submit">Search</button>
 			      	</div>
 		      	</form>
@@ -36,45 +37,11 @@
 
 
 		if (isset($_POST['user_type'])) {
-		    //$query = "SELECT firstname, lastname, email, username, block_status FROM users where user_reg_status = 0 AND user_type_id = (SELECT id FROM user_types where user_type = '".$_POST['user_type']."')";
 		    $query = "SELECT firstname, lastname, email, username, block_status FROM users INNER JOIN user_types ON (users.user_type_id = user_types.id) WHERE user_reg_status = 0 AND user_type = '".$_POST['user_type']."'";
 		    $result = $obj->select_records($query);
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-hover table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-									<td>'.$value['firstname'].'</td>
-									<td>'.$value['lastname'].'</td>
-									<td>'.$value['username'].'</td>
-									<td>'.$value['email'].'</td>
-									<td><a href="add_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Accept</button></a></td>
-									<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Reject</button></a></td>';
-
-									if($value['block_status']==0){
-										echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-										</tr>';
-									}
-								else if($value['block_status']==1)
-								{
-									echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-									</tr>';
-								}
-			    } 
-			    echo '</table></div>';
 			}
 			else
 			{
@@ -82,42 +49,12 @@
 			}
 		}
 
-		if (isset($_POST['search'])) {
-			$query = "SELECT firstname, lastname, email, username, block_status FROM users INNER JOIN user_types ON (users.user_type_id = user_types.id) WHERE user_reg_status = 1 AND firstname = '".$_POST['search']."'";
+		else if (isset($_POST['search'])) {
+			$query = "SELECT firstname, lastname, email, username, block_status FROM users INNER JOIN user_types ON (users.user_type_id = user_types.id) WHERE user_reg_status = 0 AND user_type_id != (SELECT id FROM user_types WHERE user_type = 'Admin') AND firstname LIKE '%".$_POST['search']."%'";
 		    $result = $obj->select_records($query);
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-hover table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-									<td>'.$value['firstname'].'</td>
-									<td>'.$value['lastname'].'</td>
-									<td>'.$value['username'].'</td>
-									<td>'.$value['email'].'</td>
-									<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Remove</button></a></td>';
-									if($value['block_status']==0){
-										echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-								</tr>';
-							}
-								else if($value['block_status']==1)
-								{
-									echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-								</tr>';
-								}
-			    } 
-			    echo '</table></div>';
 			}
 			else
 			{
@@ -125,47 +62,31 @@
 			}
 		}
 		
+		else if (isset($_GET['s'])) {
+			$query = "SELECT firstname, lastname, email, username, block_status FROM users INNER JOIN user_types ON (users.user_type_id = user_types.id) WHERE user_reg_status = 0 AND user_type_id != (SELECT id FROM user_types WHERE user_type = 'Admin') AND firstname LIKE '%".$_GET['s']."%'";
+		    $result = $obj->select_records($query);
+		    if($result)
+		    {
+		    	include_once 'pagination.php';
+			}
+			else
+			{
+				echo '<div style="text-align:center;"><h4 style="color : #ff0000;">There is no record for the selected user</h4></div>';
+			}
+		}
+
 		else
 		{
-			$query = "SELECT firstname, lastname, email, username, block_status FROM users where user_reg_status = 0";
+			$query = "SELECT firstname, lastname, email, username, block_status FROM users where user_reg_status = 0 AND user_type_id NOT IN (SELECT id FROM user_types WHERE user_type = 'Admin')";
 
 		    $result = $obj->select_records($query);
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-hover table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-								<td>'.$value['firstname'].'</td>
-								<td>'.$value['lastname'].'</td>
-								<td>'.$value['username'].'</td>
-								<td>'.$value['email'].'</td>
-								<td><a href="add_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Accept</button></a></td>
-								<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Reject</button></a></td>';
-
-								if($value['block_status']==0){
-									echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-									</tr>';
-								}
-							else if($value['block_status']==1)
-							{
-								echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-								</tr>';
-							}
-			    } 
-			    echo '</table></div>';
+			}
+			else
+			{
+				echo '<div style="text-align:center;"><h4 style="color : #ff0000;">There is no record</h4></div>';
 			}
 		}
 	}
--- regd_users.php
+++ regd_users.php
@@ -19,7 +19,7 @@
 	    echo '<nav class="navbar navbar-transparent justify-content-center">
 				  <form class="form-inline" method="POST" action="">
 				  	<div class="form-group">
-						<input type="text" class="form-control" id="search" value="'.$_POST['search'].'" placeholder="Enter first name" name="search">
+						<input type="text" class="form-control" id="search" value="'.($_POST['search']?$_POST['search']:$_GET['s']).'" placeholder="Enter first name" name="search">
 			      		<button class="btn btn-success form-control mr-sm-2" type="submit">Search</button>
 			      	</div>
 		      	</form>
@@ -43,36 +43,6 @@
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-									<td>'.$value['firstname'].'</td>
-									<td>'.$value['lastname'].'</td>
-									<td>'.$value['username'].'</td>
-									<td>'.$value['email'].'</td>
-									<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Remove</button></a></td>';
-									if($value['block_status']==0){
-										echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-								</tr>';
-							}
-								else if($value['block_status']==1)
-								{
-									echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-								</tr>';
-								}
-			    } 
-			    echo '</table></div>';
 			}
 			else
 			{
@@ -82,43 +52,10 @@
 
 		else if (isset($_POST['search'])) {
 			$query = "SELECT firstname, lastname, email, username, block_status FROM users INNER JOIN user_types ON (users.user_type_id = user_types.id) WHERE user_reg_status = 1 AND user_type_id != (SELECT id FROM user_types WHERE user_type = 'Admin') AND firstname LIKE '%".$_POST['search']."%'";
-
-			// print($query);
-			// exit();
 		    $result = $obj->select_records($query);
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-									<td>'.$value['firstname'].'</td>
-									<td>'.$value['lastname'].'</td>
-									<td>'.$value['username'].'</td>
-									<td>'.$value['email'].'</td>
-									<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Remove</button></a></td>';
-									if($value['block_status']==0){
-										echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-								</tr>';
-							}
-								else if($value['block_status']==1)
-								{
-									echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-								</tr>';
-								}
-			    } 
-			    echo '</table></div>';
 			}
 			else
 			{
@@ -128,43 +65,10 @@
 
 		else if (isset($_GET['s'])) {
 			$query = "SELECT firstname, lastname, email, username, block_status FROM users INNER JOIN user_types ON (users.user_type_id = user_types.id) WHERE user_reg_status = 1 AND user_type_id != (SELECT id FROM user_types WHERE user_type = 'Admin') AND firstname LIKE '%".$_GET['s']."%'";
-
-			// print($query);
-			// exit();
 		    $result = $obj->select_records($query);
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-									<td>'.$value['firstname'].'</td>
-									<td>'.$value['lastname'].'</td>
-									<td>'.$value['username'].'</td>
-									<td>'.$value['email'].'</td>
-									<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Remove</button></a></td>';
-									if($value['block_status']==0){
-										echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-								</tr>';
-							}
-								else if($value['block_status']==1)
-								{
-									echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-								</tr>';
-								}
-			    } 
-			    echo '</table></div>';
 			}
 			else
 			{
@@ -180,39 +84,11 @@
 		    if($result)
 		    {
 		    	include_once 'pagination.php';
-			    echo "<br><br>";
-			    echo "<div class='container'>";
-			    echo '<div class="table-responsive">
-							<table class="table table-bordered" align="center" style="width:90%">
-								<tr>
-									<th>First Name</th>
-									<th>Last Name</th>
-									<th>Username</th>
-									<th>Email</th>
-									<th></th>
-									<th></th>
-								</tr>';
-			    foreach ($res as $key => $value) {
-			     	echo '<tr>
-						<td>'.$value['firstname'].'</td>
-						<td>'.$value['lastname'].'</td>
-						<td>'.$value['username'].'</td>
-						<td>'.$value['email'].'</td>
-						<td><a href="remove_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Remove</button></a></td>';
-
-						if($value['block_status']==0){
-							echo '<td><a href="block_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Block</button></a></td>
-							</tr>';
-						}
-						else if($value['block_status']==1)
-						{
-							echo '<td><a href="unblock_users.php?username='.$value["username"].'&t='.$token.'"><button class="btn btn-success">Unblock</button></a></td>
-							</tr>';
-						}
-		    	} 
-			    echo '</table></div>';
 			}
-
+			else
+			{
+				echo '<div style="text-align:center;"><h4 style="color : #ff0000;">There is no record</h4></div>';
+			}
 		}
 
 	}
